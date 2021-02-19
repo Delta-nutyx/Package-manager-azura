f31c078888f0f7b1378e3a9d464a1afb051fb35e
Delta <77167689+Delta-Azura@users.noreply.github.com>
2021-02-19 15:26:38 +0100

    Script

---
+++ Script.sh
@@ -0,0 +1,15 @@
+#Bash 
+
+OPT_MODE=$1
+function install () {
+       get firefox cards.devel git gimp flameshot flcards telegram-desktop discord
+}
+
+function main () {
+       if [[ "OPT_MODE" = "-d" ]];then
+                     install 
+       fi
+} 
+main "@$" 
+            
+          
